c87f2b8b435cbd3d350b10f9596a32c28f7fcd88
Dario Fanjul <dario@MacBook-Pro-de-Dario.local>
2015-02-07 19:17:12 -0300

    Initial Commit

---
+++ touchIDExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  touchIDExample
+//
+//  Created by Dario Fanjul on 7/2/15.
+//  Copyright (c) 2015 Dario Fanjul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
