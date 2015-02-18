4f8b31cd1e234033dc98cc32503eaf8028b21b65
Dario Fanjul <dario@MacBook-Pro-de-Dario.local>
2015-02-18 20:52:58 -0300

    Sync

--- touchIDExample/ViewController.swift
+++ touchIDExample/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
+import LocalAuthentication
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func authenticatePressed(sender: AnyObject) {
+        let context = LAContext()
+        var error : NSError?
+        if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            context.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "Is you the owner?", reply: { [weak self] (success: Bool, error: NSError!) -> Void in
+                
+                if let strongSelf = self {
+                    if success {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            strongSelf.statusLabel.text = "Great! It passed"
+                        })
+                        println("Passed!")
+                    } else {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            strongSelf.statusLabel.text = error.localizedDescription
+                        })
+                        println("Error: \(error.localizedDescription)")
+                    }
+                }
+            })
+        
+        } else {
+            statusLabel.text = "Device with no touchID"
+        }
+    }
 
 }
 
